30a65bf334efa6883ab8f34fc4c01a3d63a428f4
Ember Doyle <doyle.ember@gmail.com>
2024-11-14 11:08:32 -0700

    Install parser

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors")
+const bodyParser = require('body-parser')
 const app = express();
 app.use(cors())
 
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
 
 //routes
 const indexRouter = require("./routes/indexRouter");
